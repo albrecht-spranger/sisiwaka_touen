be85919baa8ed7a4417e81dd68dcb0b40aa9c0c7
albrecht-spranger <albrecht_spranger@nifty.com>
2025-10-18 01:21:07 +0900

    2025/10/18 1:20

--- db/hp_bkup.bat
+++
@@ -1,1 +0,0 @@
-mysqldump -u root -p --default-character-set=utf8mb4  --result-file=G:\マイドライブ\20250522_南柏\sisiwaka_touen\db\bkup_sisiwaka_touen.sql sisiwaka_touen
---
+++ db/hp_bkup_table.bat
@@ -0,0 +1,1 @@
+mysqldump -u root -p --default-character-set=utf8mb4 --single-transaction --quick --hex-blob --result-file="G:\マイドライブ\00_WebSite\sisiwaka_touen\db\bkup_sisiwaka_touen.sql" sisiwaka_touen
