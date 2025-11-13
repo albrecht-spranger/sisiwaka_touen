00acc90e4d17e9ac9a6eb57cf054252a1d718ed6
albrecht-spranger <albrecht_spranger@nifty.com>
2025-11-13 22:21:35 +0900

    2025/11/13 22:21 更新情報のmoreを追加

--- scripts/top_page.js
+++ scripts/top_page.js
@@ -6,13 +6,13 @@ document.addEventListener('DOMContentLoaded', () => {
 	update_articles.forEach((text) => {
 		// 行の高さを取得
 		const style = window.getComputedStyle(text);
-		let lineHeight = parseFloat(style.lineHeight);
-		const fullHeight = text.scrollHeight;
+		let line_height = parseFloat(style.lineHeight);
+		const full_height = text.scrollHeight;
 
 		// 1行ぶんより高ければ「複数行」と判定
-		const hasMore = fullHeight > lineHeight + 1; // 誤差分 +1
+		const has_more = full_height > line_height + 1; // 誤差分 +1
 
-		if (hasMore) {
+		if (has_more) {
 			// 複数行 → 折りたたみ対象
 			text.classList.add("has_more");
 			text.classList.add("is_collapsed");
